182cd50c7c2d9b6a2f093593210193021b57b953
Paulo Rodrigues <paulor2222@gmail.com>
2025-10-20 18:04:29 -0300

    remove blank lines

--- auth_api/app/controllers/application_controller.rb
+++ auth_api/app/controllers/application_controller.rb
@@ -8,13 +8,13 @@ class ApplicationController < ActionController::API
   def authorize_request
     header = request.headers["Authorization"]
     header = header.split(" ").last if header
-    
+
     return render json: { errors: "Missing token" }, status: :unauthorized unless header
-    
+
     begin
       @decoded = JsonWebToken.decode(header)
       return render json: { errors: "Invalid token" }, status: :unauthorized unless @decoded
-      
+
       @current_user = User.find_by!(external_id: @decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
